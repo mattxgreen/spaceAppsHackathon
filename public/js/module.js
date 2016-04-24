4e615eaa8ee141e94ca7ef94aa373c44707e7dc4
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-04-24 12:40:32 -0700

    remove globe

--- public/js/module.js
+++ public/js/module.js
@@ -3,9 +3,11 @@
 
   angular
     .module("myApp", ["ui.router", "nvd3", "rzModule"])
-    .config(configFunction);
+    .config(configFunction)
+    // .config(mapsConsfig)
 
     configFunction.$inject = ['$stateProvider', '$urlRouterProvider'];
+    // mapsConfig.$inject = ['uiGmapGoogleMapApiProvider'];
 
     function configFunction($stateProvider, $urlRouterProvider) {
       $stateProvider
@@ -27,4 +29,13 @@
 
       $urlRouterProvider.otherwise("/");
     }
+
+    // function mapsConfig(uiGmapGoogleMapApiProvider) {
+    //   uiGmapGoogleMapApiProvider.configure({
+    //       //    key: 'your api key',
+    //       v: '3.20', //defaults to latest 3.X anyhow
+    //       libraries: 'weather,geometry,visualization'
+    //   });
+    // }
+
 })();
