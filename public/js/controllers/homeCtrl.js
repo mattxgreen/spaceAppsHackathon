8cd9447c8d3276393d2b6800c1e354dc4aecd74b
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-04-24 12:04:41 -0700

    add graph

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -5,9 +5,9 @@
     .module("myApp")
     .controller("homeCtrl", homeCtrl);
 
-  homeCtrl.$inject = ['$scope', 'SnapshotService', '$window']
+  homeCtrl.$inject = ['$scope', 'SnapshotService', '$window', 'GraphService']
 
-  function homeCtrl($scope, SnapshotService, $window) {
+  function homeCtrl($scope, SnapshotService, $window, GraphService) {
 
     $window.map = new google.maps.Map(document.getElementById('map'), {
       center: {
@@ -66,7 +66,72 @@
 
       });
     //
+    $scope.options = {
+        chart: {
+            type: 'scatterChart',
+            height: 450,
+            color: d3.scale.category10().range(),
+            scatter: {
+                onlyCircles: true
+            },
+            showDistX: true,
+            showDistY: true,
+          //tooltipContent: function(d) {
+          //    return d.series && '<h3>' + d.series[0].key + '</h3>';
+          //},
+            duration: 350,
+            xAxis: {
+                axisLabel: 'X Axis',
+                tickFormat: function(d){
+                    return d3.format('.02f')(d);
+                }
+            },
+            yAxis: {
+                axisLabel: 'Y Axis',
+                tickFormat: function(d){
+                    return d3.format('.02f')(d);
+                },
+                axisLabelDistance: -5
+            },
+            zoom: {
+                //NOTE: All attributes below are optional
+                enabled: true,
+                scaleExtent: [1, 10],
+                useFixedDomain: false,
+                useNiceScale: false,
+                horizontalOff: false,
+                verticalOff: false,
+                unzoomEventType: 'dblclick.zoom'
+            }
+        }
+    };
 
+    $scope.data = generateData(4,30);
+
+    /* Random Data Generator (took from nvd3.org) */
+    function generateData(groups, points) {
+        var data = [],
+            shapes = ['circle'],
+            random = d3.random.normal();
+
+        for (var i = 0; i < groups; i++) {
+            data.push({
+                key: 'Group ' + i,
+                values: []
+            });
+
+            for (var j = 0; j < points; j++) {
+                var num = Math.abs(Math.floor(random()*2));
+                data[i].values.push({
+                    x: num
+                    , y: num*20 + random()*20
+                    , size: Math.random()
+                    , shape: shapes[j % 6]
+                });
+            }
+        }
+        return data;
+    }
 
 
 
