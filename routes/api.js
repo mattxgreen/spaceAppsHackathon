989540feef1aa8a94a6ff9b3e00bbc9d24192ce3
Sergio Prada <chechopradaster@gmail.com>
2016-04-23 19:40:44 -0700

    backend

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var User = require("../models/user");
-var Snapshot = require("../models/snapshot")
+var Snapshot = require("../models/snapshot");
+var http = require('http');
+var request = require('request');
+var urlBase = 'http://www.airnowapi.org/aq/observation/latLong/current/?format=application/json'
+var dist = '&distance=25&API_KEY=' + process.env.API_KEY;
 
-var AQU = 'http://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=38.3651&longitude=-122.7&distance=25&' + process.env.API_KEY;
-
-// Basic Test response
 router.get('/', function(req, res, next) {
   console.log('api call working');
   res.send('api get')
@@ -15,16 +16,30 @@ router.get('/', function(req, res, next) {
 
 // Add new Snapshot
 router.post('/', function(req, res, next) {
-	// Req.body comes => Snapshot Schema
-	console.log(req.body);
-  Snapshot.create(req.body, function(err){
-  	if(err){
-  		return res.status(400).send(err);
-  	} 
-  		res.send(req.body);
-  });
-  
+	var snap = req.body;
+	var lat = snap.lat;
+  	var lon = snap.lon;
+  	var urlAPI = urlBase + '&latitude=' + lat + '&longitude=' + lon + dist;
+  	request(urlAPI, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        snap.api = JSON.parse(body);
+	        snap.airNowData = true;
+	        Snapshot.create(snap, function(err){
+  				if(err) return res.status(400).send(err);
+  				return res.send(snap);
+  			});
+	    }
+	    else {
+	    	snap.airNowData = false;
+	    	Snapshot.create(snap, function(err){
+  				if(err) return res.status(400).send(err);
+  				return res.send();
+  			});
+	    }
+	});
 });
+
+
 
 router.get('/geo/:loc', function(req, res, next){
 	Snapshot.find({})
@@ -32,6 +47,11 @@ router.get('/geo/:loc', function(req, res, next){
 
 		})
 })
+
+
+
+
+
 
 
 
